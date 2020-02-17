93549a30efc7a8a2cbe5b7c324c9aa776a031b1d
J. A. Reyna Espinoza <andresreyna15@gmail.com>
2020-02-17 12:29:02 -0600

    First commit

---
+++ src/firstproject/FirstProject.java
@@ -0,0 +1,33 @@
+package firstproject;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Alumno
+ */
+public class FirstProject extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root, Color.TRANSPARENT);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
